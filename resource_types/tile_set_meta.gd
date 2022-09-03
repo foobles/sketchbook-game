42fe2f1750ea6cfe7af96cf3093c6dadd7e6a830
April <buzdav2@gmail.com>
2022-09-03 12:30:16 -0700

    fix incomplete height array for tile; add assertions for height array lengths

--- resource_types/tile_set_meta.gd
+++ resource_types/tile_set_meta.gd
@@ -10,6 +10,8 @@ func into_array():
 	ret.resize(tile_set.get_last_unused_tile_id())
 	for attribute in tile_attributes:
 		attribute.compute_widths()
+		assert(len(attribute.heights) == 16)
+		assert(len(attribute.widths) == 16)
 		var idx = tile_set.find_tile_by_name(attribute.name)
 		ret[idx] = attribute 
 
