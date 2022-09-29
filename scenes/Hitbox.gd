c39f350764e2f774b502ed6824335f168322fe82
April <buzdav2@gmail.com>
2022-09-28 21:54:02 -0700

    add collision detection between hitboxes

--- scenes/Hitbox.gd
+++ scenes/Hitbox.gd
@@ -12,6 +12,19 @@ func set_radius(new_radius):
 	radius = new_radius 
 	update()
 
+
+func collides_with(other):
+	var combined_radius = radius + other.radius
+	var diff = global_position.floor() - (other.global_position.floor() - combined_radius)
+	var combined_diameter = 2*combined_radius
+	return (
+		diff.x >= 0
+		&& diff.x <= combined_diameter.x
+		&& diff.y >= 0
+		&& diff.y <= combined_diameter.y
+	)
+
+
 func _draw():
 	var center = to_local(global_position.floor())
 	draw_rect(
