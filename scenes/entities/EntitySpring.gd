90cd2f77e3ebb71ab6ff32ee4b64fc9014e4719a
April <buzdav2@gmail.com>
2023-01-06 21:21:38 -0600

    add springs

---
+++ scenes/entities/EntitySpring.gd
@@ -0,0 +1,79 @@
+tool
+extends "res://scenes/entities/EntitySolid.gd"
+
+const PULL_IN_DISTANCE = 8
+const CONTROL_LOCK_FRAMES = 16
+
+enum Direction {
+	RIGHT = 0,
+	UP = 1,
+	LEFT = 2,
+	DOWN = 3
+}
+
+enum Power {
+	STRONG,
+	WEAK
+}
+
+export(Direction) var push_direction = Direction.UP setget set_push_direction 
+export(Power) var push_power = Power.STRONG setget set_push_power
+
+const _HB_WIDTH = 15
+const _HB_HEIGHT = 8
+
+func _ready():
+	match push_direction:
+		Direction.RIGHT, Direction.LEFT:
+			self.connect("player_collided_horizontal", self, "trigger_push")
+		Direction.DOWN:
+			self.connect("player_collided_bottom", self, "trigger_push")
+		Direction.UP:
+			self.connect("player_landed_top", self, "trigger_push")
+			
+
+func trigger_push(player):
+	var push_speed = get_push_speed()
+	player.transition_become_airborne()
+	match push_direction:
+		Direction.RIGHT:
+			player.velocity.x = push_speed
+			player.position.x -= PULL_IN_DISTANCE
+			player.facing_direction = +1
+			
+		Direction.UP:
+			player.velocity.y = -push_speed
+			player.position.y += PULL_IN_DISTANCE
+			
+		Direction.LEFT:
+			player.velocity.x = -push_speed
+			player.position.x += PULL_IN_DISTANCE
+			player.facing_direction = -1
+			
+		Direction.DOWN:
+			player.velocity.y = push_speed
+			player.position.y -= PULL_IN_DISTANCE
+	
+	match push_power:
+		Power.WEAK: $AnimationPlayer.play("trigger_weak")
+		Power.STRONG: $AnimationPlayer.play("trigger_strong")
+
+
+func set_push_direction(new_push_direction):
+	push_direction = new_push_direction
+	$Sprite.rotation_degrees = -90 * (push_direction-1)
+	if push_direction & 1 == 1:
+		$Hitbox.radius = Vector2(_HB_WIDTH, _HB_HEIGHT)
+	else:
+		$Hitbox.radius = Vector2(_HB_HEIGHT, _HB_WIDTH)
+
+
+func set_push_power(new_push_power):
+	push_power = new_push_power
+	$Sprite.frame = push_power * 3
+	
+	
+func get_push_speed():
+	match push_power:
+		Power.WEAK: return 10
+		Power.STRONG: return 16
